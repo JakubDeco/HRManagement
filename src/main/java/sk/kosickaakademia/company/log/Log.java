5dbf6a783e8093a6a69ee92158e0058d5e22aebc
JakubDeco <jakub.deco@kosickaakademia.sk>
2021-03-02 13:29:07 +0100

    added Log.error and Log.print

---
+++ src/main/java/sk/kosickaakademia/company/log/Log.java
@@ -0,0 +1,11 @@
+package sk.kosickaakademia.company.log;
+
+public abstract class Log {
+    public static void error(String str){
+        System.out.println("[ERROR] - "+str);
+    }
+
+    public static void print(String str){
+        System.out.println("[OK] - "+str);
+    }
+}
