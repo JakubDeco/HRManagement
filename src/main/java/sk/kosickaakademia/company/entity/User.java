eea0ba3c8f30f66cc9423237883f611e283004a1
JakubDeco <jakub.deco@kosickaakademia.sk>
2021-03-02 10:43:19 +0100

    added enum Gender; updated datatype of User.gender to Gender and updated constructors

--- src/main/java/sk/kosickaakademia/company/entity/User.java
+++ src/main/java/sk/kosickaakademia/company/entity/User.java
@@ -1,24 +1,43 @@
 package sk.kosickaakademia.company.entity;
 
+import sk.kosickaakademia.company.enumerator.Gender;
+
 public class User {
     private int id;
     private String fName;
     private String lName;
     private int name;
-    private boolean gender;
+    private Gender gender;
 
-    public User(int id, String fName, String lName, int name, boolean gender) {
-        this.id = id;
+    public User(String fName, String lName, int name, int gender) {
         this.fName = fName;
         this.lName = lName;
         this.name = name;
-        this.gender = gender;
+        this.gender = gender==0 ? Gender.MALE : gender == 1 ? Gender.FEMALE : Gender.OTHER;
     }
 
-    public User(String fName, String lName, int name, boolean gender) {
-        this.fName = fName;
-        this.lName = lName;
-        this.name = name;
-        this.gender = gender;
+    public User(int id, String fName, String lName, int name, int gender) {
+        this(fName, lName, name, gender);
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getfName() {
+        return fName;
+    }
+
+    public String getlName() {
+        return lName;
+    }
+
+    public int getName() {
+        return name;
+    }
+
+    public Gender getGender() {
+        return gender;
     }
 }
