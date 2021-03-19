7d2c71d8e30e91bc480db41b22745fe3ef825310
JakubDeco <jakub.deco@kosickaakademia.sk>
2021-03-19 12:02:09 +0100

    added SecretController.getSecret

--- src/main/java/sk/kosickaakademia/company/controller/SecretController.java
+++ src/main/java/sk/kosickaakademia/company/controller/SecretController.java
@@ -4,10 +4,7 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import sk.kosickaakademia.company.util.Status;
 import sk.kosickaakademia.company.util.Util;
 
@@ -20,8 +17,17 @@ public class SecretController {
     private Map<String, String> loggedUsers = new HashMap<>();
 
     @GetMapping(path = "/secret")
-    public ResponseEntity<String> getSecret(){
-        return null;
+    public ResponseEntity<String> getSecret(@RequestHeader("token") String token){
+        String secret = "This is our secret!!!";
+        for (Map.Entry<String, String> entry: loggedUsers.entrySet()){
+            if (("Bearer "+entry.getValue()).equals(token)) {
+                JSONObject response = new JSONObject();
+                response.put("secret", secret);
+                return Status.status200(response);
+            }
+        }
+
+        return Status.status401("Invalid token.");
     }
 
     @PostMapping(path = "/login")
